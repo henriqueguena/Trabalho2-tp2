a9cda5b3f8c81ff8c745416366bd187823ecbab6
unknown <henriqueguena@gmail.com>
2024-08-03 18:05:19 -0300

    parte 1 completa

--- src/main/scala/br/unb/cic/flang/Monad.scala
+++ src/main/scala/br/unb/cic/flang/Monad.scala
@@ -10,11 +10,8 @@ object StateMonad {
 
   def bind[A, B](m: M[A])(f: A => M[B]): M[B] = m.flatMap(f)
 
-  // Reimplementa o `runState` com a estrutura desejada
   def runState[A](state: M[A], initial: S): (A, S) = {
-    // Executa o state com o estado inicial e obtém a tupla (S, A)
     val (finalState, result) = state.run(initial).value
-    // Retorna a tupla (A, S) conforme o formato desejado
     (result, finalState)
   }
 
