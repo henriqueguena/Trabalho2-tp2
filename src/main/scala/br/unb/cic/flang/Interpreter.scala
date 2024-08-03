bd1237028266f9c8cebde2cc1970755f4c21cb04
unknown <henriqueguena@gmail.com>
2024-08-03 19:43:29 -0300

    Suporte para and or e diferente

--- src/main/scala/br/unb/cic/flang/Interpreter.scala
+++ src/main/scala/br/unb/cic/flang/Interpreter.scala
@@ -5,31 +5,37 @@ import StateMonad._
 
 object Interpreter {
 
-  def eval(expr: Expr, declarations: List[FDeclaration]): M[Integer] =
+  def eval(expr: Expr, declarations: List[FDeclaration]): M[Either[Boolean, Integer]] =
     expr match {
-      case CInt(v) => pure(v)
-      case CBool(b) => throw new IllegalArgumentException("Boolean expressions should not be evaluated to integers")
+      case CInt(v) => pure(Right(v))
+      case CBool(b) => pure(Left(b))
       case Add(lhs, rhs) =>
         bind(eval(lhs, declarations)) { l =>
           bind(eval(rhs, declarations)) { r =>
-            pure(l + r)
+            (l, r) match {
+              case (Right(li), Right(ri)) => pure(Right(li + ri))
+              case _ => throw new IllegalArgumentException("Add operation requires integer operands")
+            }
           }
         }
       case Mul(lhs, rhs) =>
         bind(eval(lhs, declarations)) { l =>
           bind(eval(rhs, declarations)) { r =>
-            pure(l * r)
+            (l, r) match {
+              case (Right(li), Right(ri)) => pure(Right(li * ri))
+              case _ => throw new IllegalArgumentException("Mul operation requires integer operands")
+            }
           }
         }
       case Id(name) =>
         bind(get()) { state =>
-          pure(lookupVar(name, state))
+          pure(Right(lookupVar(name, state)))
         }
       case App(name, arg) =>
         val fdecl = lookup(name, declarations)
         bind(eval(arg, declarations)) { value =>
           bind(get()) { s1 =>
-            bind(put(declareVar(fdecl.arg, value, s1))) { s2 =>
+            bind(put(declareVar(fdecl.arg, value.fold(_ => throw new IllegalArgumentException("Function argument must be integer"), identity), s1))) { s2 =>
               eval(fdecl.body, declarations)
             }
           }
@@ -37,32 +43,47 @@ object Interpreter {
       case Eq(lhs, rhs) =>
         bind(eval(lhs, declarations)) { l =>
           bind(eval(rhs, declarations)) { r =>
-            pure(if (l == r) 1 else 0)
+            (l, r) match {
+              case (Right(li), Right(ri)) => pure(Left(li == ri))
+              case (Left(lb), Left(rb)) => pure(Left(lb == rb))
+              case _ => throw new IllegalArgumentException("Eq operation requires operands of the same type")
+            }
+          }
+        }
+      case Different(lhs, rhs) =>
+        bind(eval(lhs, declarations)) { l =>
+          bind(eval(rhs, declarations)) { r =>
+            (l, r) match {
+              case (Right(li), Right(ri)) => pure(Left(li != ri))
+              case (Left(lb), Left(rb)) => pure(Left(lb != rb))
+              case _ => throw new IllegalArgumentException("Different operation requires operands of the same type")
+            }
           }
         }
       case IfThenElse(cond, thenBranch, elseBranch) =>
-        bind(evalCondition(cond, declarations)) { condition =>
-          if (condition) eval(thenBranch, declarations) else eval(elseBranch, declarations)
+        bind(eval(cond, declarations)) {
+          case Left(condition) =>
+            if (condition) eval(thenBranch, declarations)
+            else eval(elseBranch, declarations)
+          case _ => throw new IllegalArgumentException("Condition must be a boolean")
         }
-    }
-
-
-  def evalCondition(expr: Expr, declarations: List[FDeclaration]): M[Boolean] =
-    expr match {
-      case CInt(v) =>
-        v match {
-          case _ if v.equals(Integer.valueOf(0)) => pure(false)
-          case _ if v.equals(Integer.valueOf(1)) => pure(true)
-          case _ => throw new IllegalArgumentException("Integer values other than 0 or 1 cannot be evaluated as Boolean")
-        }
-      case CBool(v) => pure(v)
-      case Eq(lhs, rhs) =>
+      case Or(lhs, rhs) =>
         bind(eval(lhs, declarations)) { l =>
           bind(eval(rhs, declarations)) { r =>
-            pure(l == r)
+            (l, r) match {
+              case (Left(lb), Left(rb)) => pure(Left(lb || rb))
+              case _ => throw new IllegalArgumentException("Or operation requires boolean operands")
+            }
           }
         }
-      case _ => throw new IllegalArgumentException("Invalid condition expression")
+      case And(lhs, rhs) =>
+        bind(eval(lhs, declarations)) { l =>
+          bind(eval(rhs, declarations)) { r =>
+            (l, r) match {
+              case (Left(lb), Left(rb)) => pure(Left(lb && rb))
+              case _ => throw new IllegalArgumentException("And operation requires boolean operands")
+            }
+          }
+        }
     }
 }
-
--- src/test/scala/br/unb/cic/flang/InterpreterTest.scala
+++ src/test/scala/br/unb/cic/flang/InterpreterTest.scala
@@ -11,74 +11,114 @@ import StateMonad._
 class InterpreterTest extends AnyFlatSpec with should.Matchers {
 
   val inc = FDeclaration("inc", "x", Add(Id("x"), CInt(1)))
-
   val declarations = List(inc)
+  val initialState: S = List("x" -> 42)
 
-  val initialState: S = List()
+  "eval Id(x)" should "return Right(42) when x is bound to 42" in {
+    val id = Id("x")
+    val (res, _) = runState(eval(id, declarations), initialState)
+    res should be (Right(42))
+  }
 
-  "eval CInt(5)" should "return an integer value 5." in {
+  "eval CInt(5)" should "return Right(5)" in {
     val c5 = CInt(5)
     val (res, _) = runState(eval(c5, declarations), initialState)
-    res should be (5)
+    res should be (Right(5))
   }
 
-  "eval Add(CInt(5), CInt(10)) " should "return an integer value 15." in {
-    val c5  = CInt(5)
-    val c10 = CInt(10)
-    val add = Add(c5, c10)
+  "eval CBool(true)" should "return Left(true)" in {
+    val cTrue = CBool(true)
+    val (res, _) = runState(eval(cTrue, declarations), initialState)
+    res should be (Left(true))
+  }
+
+  "eval Add(CInt(5), CInt(10))" should "return Right(15)" in {
+    val add = Add(CInt(5), CInt(10))
     val (res, _) = runState(eval(add, declarations), initialState)
-    res should be (15)
+    res should be (Right(15))
   }
 
-  "eval Add(CInt(5), Add(CInt(5), CInt(10))) " should "return an integer value 20." in {
-    val c5 = CInt(5)
-    val c10 = CInt(10)
-    val add = Add(c5, Add(c5, c10))
-    val (res, _) = runState(eval(add, declarations), initialState)
-    res should be(20)
-  }
-
-  "eval Mul(CInt(5), CInt(10))" should "return an integer value 50" in {
-    val c5 = CInt(5)
-    val c10 = CInt(10)
-    val mul = Mul(c5, CInt(10))
+  "eval Mul(CInt(5), CInt(10))" should "return Right(50)" in {
+    val mul = Mul(CInt(5), CInt(10))
     val (res, _) = runState(eval(mul, declarations), initialState)
-    res should be(50)
+    res should be (Right(50))
   }
 
-  "eval App(inc, 99) " should "return an integer value 100" in {
+  "eval App(inc, CInt(99))" should "return Right(100)" in {
     val app = App("inc", CInt(99))
     val (res, _) = runState(eval(app, declarations), initialState)
-    res should be (100)
+    res should be (Right(100))
   }
 
-  "eval IfThenElse(Eq(CInt(1), CInt(1)), CInt(1), CInt(0))" should "return 1 (true)" in {
-    val ifElse = IfThenElse(Eq(CInt(1), CInt(1)), CInt(1), CInt(0))
-    val (res, _) = runState(eval(ifElse, declarations), initialState)
-    res should be (1)
+  "eval Eq(CInt(1), CInt(1))" should "return Left(true)" in {
+    val eq = Eq(CInt(1), CInt(1))
+    val (res, _) = runState(eval(eq, declarations), initialState)
+    res should be (Left(true))
   }
 
-  "eval IfThenElse(Eq(CInt(1), CInt(0)), CInt(1), CInt(0))" should "return 0 (false)" in {
-    val ifElse = IfThenElse(Eq(CInt(1), CInt(0)), CInt(1), CInt(0))
-    val (res, _) = runState(eval(ifElse, declarations), initialState)
-    res should be (0)
+  "eval Eq(CInt(1), CInt(0))" should "return Left(false)" in {
+    val eq = Eq(CInt(1), CInt(0))
+    val (res, _) = runState(eval(eq, declarations), initialState)
+    res should be (Left(false))
   }
 
-  "eval IfThenElse(Eq(Add(CInt(1), CInt(1)), CInt(2)), CInt(100), CInt(200))" should "return 100 (true)" in {
-    val ifElse = IfThenElse(Eq(Add(CInt(1), CInt(1)), CInt(2)), CInt(100), CInt(200))
-    val (res, _) = runState(eval(ifElse, declarations), initialState)
-    res should be (100)
+  "eval Different(CInt(1), CInt(1))" should "return Left(false)" in {
+    val diff = Different(CInt(1), CInt(1))
+    val (res, _) = runState(eval(diff, declarations), initialState)
+    res should be (Left(false))
   }
 
-  "eval IfThenElse(Eq(Add(CInt(2), CInt(2)), CInt(5)), CInt(100), CInt(200))" should "return 200 (false)" in {
-    val ifElse = IfThenElse(Eq(Add(CInt(2), CInt(2)), CInt(5)), CInt(100), CInt(200))
-    val (res, _) = runState(eval(ifElse, declarations), initialState)
-    res should be (200)
+  "eval Different(CInt(1), CInt(0))" should "return Left(true)" in {
+    val diff = Different(CInt(1), CInt(0))
+    val (res, _) = runState(eval(diff, declarations), initialState)
+    res should be (Left(true))
   }
 
-  "eval IfThenElse(CInt(1), CInt(100), CInt(200))" should "return 100 (true)" in {
-    val ifElse = IfThenElse(CInt(1), CInt(100), CInt(200))
+  "eval Different(CBool(true), CBool(false))" should "return Left(true)" in {
+    val diff = Different(CBool(true), CBool(false))
+    val (res, _) = runState(eval(diff, declarations), initialState)
+    res should be (Left(true))
+  }
+
+  "eval Different(CBool(true), CBool(true))" should "return Left(false)" in {
+    val diff = Different(CBool(true), CBool(true))
+    val (res, _) = runState(eval(diff, declarations), initialState)
+    res should be (Left(false))
+  }
+
+  "eval IfThenElse(CBool(true), CInt(100), CInt(200))" should "return Right(100)" in {
+    val ifElse = IfThenElse(CBool(true), CInt(100), CInt(200))
     val (res, _) = runState(eval(ifElse, declarations), initialState)
-    res should be (100)
+    res should be (Right(100))
+  }
+
+  "eval IfThenElse(CBool(false), CInt(100), CInt(200))" should "return Right(200)" in {
+    val ifElse = IfThenElse(CBool(false), CInt(100), CInt(200))
+    val (res, _) = runState(eval(ifElse, declarations), initialState)
+    res should be (Right(200))
+  }
+
+  "eval Or(CBool(true), CBool(false))" should "return Left(true)" in {
+    val or = Or(CBool(true), CBool(false))
+    val (res, _) = runState(eval(or, declarations), initialState)
+    res should be (Left(true))
+  }
+
+  "eval Or(CBool(false), CBool(false))" should "return Left(false)" in {
+    val or = Or(CBool(false), CBool(false))
+    val (res, _) = runState(eval(or, declarations), initialState)
+    res should be (Left(false))
+  }
+
+  "eval And(CBool(true), CBool(true))" should "return Left(true)" in {
+    val and = And(CBool(true), CBool(true))
+    val (res, _) = runState(eval(and, declarations), initialState)
+    res should be (Left(true))
+  }
+
+  "eval And(CBool(true), CBool(false))" should "return Left(false)" in {
+    val and = And(CBool(true), CBool(false))
+    val (res, _) = runState(eval(and, declarations), initialState)
+    res should be (Left(false))
   }
 }
